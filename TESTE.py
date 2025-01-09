0380cf4af2e7ec1f9a12cef88b7154f891737419
criswp <104799341+Cristianwp21@users.noreply.github.com>
2025-01-09 02:34:36 -0300

    TESTE.py

--- TESTE.py
+++ TESTE.py
@@ -1,12 +1,15 @@
 from telegram import Bot
 
-# Substitua pelo token do BotFather
 TELEGRAM_TOKEN = "7676787815:AAHLOCqbmuQgJN8NDgxvsgnyoH_nMhWCbyM"
 CHAT_ID = "1722310941"  # Chat ID obtido do getUpdates
 
 def enviar_alerta(mensagem):
-    bot = Bot(token=TELEGRAM_TOKEN)
-    bot.send_message(chat_id=CHAT_ID, text=mensagem)
+    try:
+        bot = Bot(token=TELEGRAM_TOKEN)
+        bot.send_message(chat_id=CHAT_ID, text=mensagem)
+        print("Mensagem enviada com sucesso!")
+    except Exception as e:
+        print(f"Erro ao enviar mensagem: {e}")
 
 # Teste o envio
 enviar_alerta("Teste de alerta do bot!")
